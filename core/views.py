f5784c80ec1801c8d5a08339384e1ac123da787c
¨SantiG¨ <santigavinoutn@gmail.com>
2026-05-26 15:52:40 -0300

    Inicio frontend Django

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def health_check(request):
-    return HttpResponse("OK")
+def home(request):
+    return render(request, "core/index.html")
